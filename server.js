168a60c289832ff2371f27aef544f5ab6f84d439
Patrick Sawyer <patrick_sawyer@hotmail.co.uk>
2021-05-01 10:52:34 +0100

    test1

---
+++ server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static('build'));
+app.listen(PORT, () => console.log('listening on port ' + PORT));
